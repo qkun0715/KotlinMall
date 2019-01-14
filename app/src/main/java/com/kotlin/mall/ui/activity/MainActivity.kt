c1b9ba425a8bb21397bcf739fcc55c942aaea102
huangqiankun123 <18671166183@163.com>
2019-01-14 22:02:47 +0800

    资源文件的导入  封装底部栏

--- app/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
+++ app/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
@@ -3,11 +3,16 @@ package com.kotlin.mall.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.mall.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mBottomNavBar.run {
+            checkMsgBadge(false)
+            checkCartBadge(10)
+        }
     }
 }
