46e568c7c662342e7635a41000c9d18552c73467
huangqiankun123 <18671166183@163.com>
2019-01-17 17:51:44 +0800

    错误修改

--- UserCenter/src/main/java/com/kotlin/user/service/impl/UserServiceImpl.kt
+++ UserCenter/src/main/java/com/kotlin/user/service/impl/UserServiceImpl.kt
@@ -10,11 +10,16 @@ import rx.functions.Func1
 class UserServiceImpl : UserService {
     override fun register(mobile: String, pwd: String, verifyCode: String): Observable<Boolean> {
         val repository = UserRepository()
-        return Observable.just(true)
+        return repository.register(mobile, pwd, verifyCode)
+            .flatMap {
+                return@flatMap Observable.just(true)
+            }
+
+
 //        return repository.register(mobile, pwd, verifyCode)
 //            .flatMap(object :Func1<BaseResp<String>,Observable<Boolean>?>{
 //                override fun call(t: BaseResp<String>?): Observable<Boolean> {
-//                    if (t.status!=0){
+//                    if (t?.status!=0){
 //                        return Observable.error(BaseException(t.status,t.message))
 //                    }
 //                    return Observable.just(true)
